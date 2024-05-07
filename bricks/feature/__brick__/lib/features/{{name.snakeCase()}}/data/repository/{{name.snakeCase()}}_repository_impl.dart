f460569d950cd478456f9e69043cf756a8e45f57
umkithya <kithya678@gmail.com>
2024-05-07 10:02:14 +0700

    update

--- bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/data/repository/{{name.snakeCase()}}_repository_impl.dart
+++ bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/data/repository/{{name.snakeCase()}}_repository_impl.dart
@@ -2,6 +2,7 @@ import '../../../../core/resource/data_state.dart';
 import '../../domain/repository/{{name.snakeCase()}}_repository.dart';
 import '../data_sources/remote/{{name.snakeCase()}}_remote_data_source.dart';
 import '../model/params/{{name.snakeCase()}}_param.dart';
+import '../../domain/entity/{{name.snakeCase()}}_entity.dart';
 
 class {{name.pascalCase()}}RepositoryImpl implements I{{name.pascalCase()}}Repository {
   final I{{name.pascalCase()}}RemoteDataSource _{{name.camelCase()}}RemoteDataSource;
